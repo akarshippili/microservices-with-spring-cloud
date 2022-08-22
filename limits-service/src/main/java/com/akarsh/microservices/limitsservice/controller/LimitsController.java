576794f9e646be27e7039c9f62e1b099b0a713e4
IPPILIAKARSH <ippiliakarsh@gmail.com>
2022-08-23 02:00:27 +0530

    added limits endpoint

--- limits-service/src/main/java/com/akarsh/microservices/limitsservice/controller/LimitsController.java
+++ limits-service/src/main/java/com/akarsh/microservices/limitsservice/controller/LimitsController.java
@@ -1,7 +1,19 @@
 package com.akarsh.microservices.limitsservice.controller;
 
+import com.akarsh.microservices.limitsservice.bean.Limits;
+import com.akarsh.microservices.limitsservice.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping(path = "/limits")
+    public Limits retrieveLimits(){
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+    }
+
 }
