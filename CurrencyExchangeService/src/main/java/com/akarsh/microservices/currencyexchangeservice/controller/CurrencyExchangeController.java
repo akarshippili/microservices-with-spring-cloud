9c3ada86af2a2753c5a2fa078142f45f33e76a92
IPPILIAKARSH <ippiliakarsh@gmail.com>
2022-08-27 02:10:50 +0530

    Update CurrencyExchangeController.java

--- CurrencyExchangeService/src/main/java/com/akarsh/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ CurrencyExchangeService/src/main/java/com/akarsh/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,6 +1,7 @@
 package com.akarsh.microservices.currencyexchangeservice.controller;
 
 import com.akarsh.microservices.currencyexchangeservice.model.CurrencyExchange;
+import com.akarsh.microservices.currencyexchangeservice.repo.CurrencyExchangeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,9 +18,18 @@ public class CurrencyExchangeController {
     @Autowired
     private Environment environment;
 
-    @GetMapping(path = "/from/{source}/to/{dest}")
-    public CurrencyExchange retrieveExchangeValue(@PathVariable String source, @PathVariable String dest){
-        return new CurrencyExchange(1l, source, dest, BigDecimal.valueOf(65.005), environment.getProperty("local.server.port"));
+    @Autowired
+    private CurrencyExchangeRepository repository;
+
+    @GetMapping(path = "/from/{source}/to/{target}")
+    public CurrencyExchange retrieveExchangeValue(@PathVariable String source, @PathVariable String target){
+        CurrencyExchange currencyExchange = repository.findBySourceAndTarget(source, target);
+
+        if(currencyExchange==null) throw new RuntimeException("Invalid source or Invalid target");
+
+        currencyExchange.setEnvironment(environment.getProperty("local.server.port"));
+        return currencyExchange;
+//        return new CurrencyExchange(1l, source, dest, BigDecimal.valueOf(65.005), environment.getProperty("local.server.port"));
     }
 
 }
