03e78edcc7cec327ba9f7fb74f986f5319c61c68
IPPILIAKARSH <ippiliakarsh@gmail.com>
2022-08-27 15:07:09 +0530

    added v2 calculateConversion using  Feign client

--- CurrencyConversionService/src/main/java/com/akarsh/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ CurrencyConversionService/src/main/java/com/akarsh/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,10 +1,9 @@
 package com.akarsh.microservices.currencyconversionservice.controller;
 
+import com.akarsh.microservices.currencyconversionservice.CurrencyExchangeProxy;
 import com.akarsh.microservices.currencyconversionservice.model.CurrencyConversion;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.MimeType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +19,7 @@ import java.util.Map;
 public class CurrencyConversionController {
 
     @Autowired
-    private Environment environment;
+    private CurrencyExchangeProxy currencyExchangeProxy;
 
     @GetMapping(path = "from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateConversion(
@@ -42,6 +41,29 @@ public class CurrencyConversionController {
         System.out.println(response);
 
         CurrencyConversion currencyConversion = response.getBody();
+
+        return new CurrencyConversion(
+                currencyConversion.getId(),
+                from,
+                to,
+                quantity,
+                currencyConversion.getConversionMultiple(),
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment());
+    }
+
+    @GetMapping(path = "v2/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateConversionFeign(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity
+    ){
+
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+
+        CurrencyConversion currencyConversion = currencyExchangeProxy.retrieveExchangeValue(from, to);
 
         return new CurrencyConversion(
                 currencyConversion.getId(),
