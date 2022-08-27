fabe507176d8a38f9fe959510c87aa2e445fb915
IPPILIAKARSH <ippiliakarsh@gmail.com>
2022-08-27 15:45:27 +0530

    Create CurrencyExchangeProxy.java

---
+++ CurrencyConversionService/src/main/java/com/akarsh/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -0,0 +1,15 @@
+package com.akarsh.microservices.currencyconversionservice;
+
+import com.akarsh.microservices.currencyconversionservice.model.CurrencyConversion;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.math.BigDecimal;
+
+@FeignClient(name = "currency-exchange", url = "localhost:8000")
+public interface CurrencyExchangeProxy {
+
+    @GetMapping(path = "/currency-exchange/from/{source}/to/{target}")
+    public CurrencyConversion retrieveExchangeValue(@PathVariable String source, @PathVariable String target);
+}
