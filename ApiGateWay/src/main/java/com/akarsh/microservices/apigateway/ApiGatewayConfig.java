b175a4e94891a6cd703bcd237a0b149ec2672cb1
IPPILIAKARSH <ippiliakarsh@gmail.com>
2022-08-31 15:42:42 +0530

    added RouteLocator
    for routing

---
+++ ApiGateWay/src/main/java/com/akarsh/microservices/apigateway/ApiGatewayConfig.java
@@ -0,0 +1,20 @@
+package com.akarsh.microservices.apigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfig {
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder){
+        return builder
+                .routes()
+                .route(predicateSpec -> predicateSpec.path("/currency-exchange/**").uri("lb://currency-exchange"))
+                .route(predicateSpec -> predicateSpec.path("/currency-conversion/**").uri("lb://currency-conversion"))
+                .build();
+    }
+
+}
